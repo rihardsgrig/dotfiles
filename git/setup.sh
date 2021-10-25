7867d8dc6e96fefbb4b8405c87c2c1ea3d6eaf75
Rihards Grigorjevs <rihards.grigorjevs@gmail.com>
2021-10-25 16:13:46 +0300

    update git-sync

--- git/setup.sh
+++ git/setup.sh
@@ -27,30 +27,31 @@ SCRIPT_URL="https://raw.githubusercontent.com/simonthum/git-sync/master/git-sync
 
 
 if [[ -a "$SCRIPT_PATH" ]]; then
-  substep_info "You already have git-sync installed. Delete $SCRIPT_PATH if you want to download the latest version."
-else
-  # download git-sync
-  substep_info "Fetching script..."
-  mkdir -p $SCRIPT_LOC
-  curl -sL $SCRIPT_URL > $SCRIPT_PATH && substep_info "Saved to ${SCRIPT_PATH}."
-  chmod +x $SCRIPT_PATH
+  substep_info "You already have git-sync installed. Removing $SCRIPT_PATH older version."
+  rm -rf $SCRIPT_PATH
+fi
 
-  # detect SCRIPT_LOC in path
-  IFS=":"
-  FOUND_PATH=""
-  for p in $PATH; do
-    if [[ "$p" = "$SCRIPT_LOC" ]]; then
-      FOUND_PATH="true"
-      substep_info "Found ${SCRIPT_LOC} in \$PATH. All set!" >&2
-    fi
-  done
+# download git-sync
+substep_info "Fetching script..."
+mkdir -p $SCRIPT_LOC
+curl -sL $SCRIPT_URL > $SCRIPT_PATH && substep_info "Saved to ${SCRIPT_PATH}."
+chmod +x $SCRIPT_PATH
 
-  substep_success "Successfully installed git-sync."
-
-  # tell user to adjust $PATH if necessary
-  if [[ -z "$FOUND_PATH" ]]; then
-    substep_info "Now add ${SCRIPT_LOC} to \$PATH."
+# detect SCRIPT_LOC in path
+IFS=":"
+FOUND_PATH=""
+for p in $PATH; do
+  if [[ "$p" = "$SCRIPT_LOC" ]]; then
+    FOUND_PATH="true"
+    substep_info "Found ${SCRIPT_LOC} in \$PATH. All set!" >&2
   fi
+done
+
+substep_success "Successfully installed git-sync."
+
+# tell user to adjust $PATH if necessary
+if [[ -z "$FOUND_PATH" ]]; then
+  substep_info "Now add ${SCRIPT_LOC} to \$PATH."
 fi
 
 success "git-sync is set up."
